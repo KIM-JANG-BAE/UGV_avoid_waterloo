7e995bc30473a5ebd9f5fdf204fe006a0fab6ef4
KIM-JANG-BAE <100831121+KIM-JANG-BAE@users.noreply.github.com>
2024-08-09 00:19:45 +0900

    Add files via upload

---
+++ s1.py
@@ -0,0 +1,84 @@
+# Receives images and runs them through ML algorithm
+
+import socket
+import struct
+import cv2
+import pickle
+from time import sleep
+# for demonstration
+import matplotlib.pyplot as plt
+
+HOST = '0.0.0.0'
+PORT = 5555
+
+flag = True
+
+def show_image(frame):
+    cv2.imshow('image', frame)
+
+def send_data(socket, frame):
+    s = 'hello'
+    s = s.encode()
+    socket.send(s)
+
+def main():
+    global flag
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST, PORT))
+    s.listen(1)
+
+    print("Server started")
+    while True:
+        client_socket, addr = s.accept()
+        print("Connected by", addr)
+
+        try: 
+            while True:
+                
+                print('server 1')
+
+                frame_size_bytes = client_socket.recv(4)
+                frame_size = int.from_bytes(frame_size_bytes, byteorder='big')
+    
+                print(frame_size)
+
+                print('server 2')
+
+
+
+                if frame_size == 0:
+                    continue
+            
+
+                data = b''
+                while len(data) < frame_size:
+                    recv = client_socket.recv(4096)
+                    if not recv:
+                        break
+                    data += recv
+                
+                print('server 3')
+
+                _, frame = pickle.loads(data)
+
+
+                print(frame)
+                print('server 4')
+
+                if flag:
+                    flag = False
+                    show_image(frame)
+
+                send_data(client_socket, frame)
+
+                print('server 5')
+                frame_size = 0
+
+        except Exception as e:
+            print('hello')
+            print(e)
+        finally:
+            client_socket.close()
+
+if __name__ == "__main__":
+    main()
