b04381925f175677e9bcf807bf62c3c92b2a71df
KIM-JANG-BAE <100831121+KIM-JANG-BAE@users.noreply.github.com>
2024-08-09 05:19:41 +0900

    Update s1.py

--- s1.py
+++ s1.py
@@ -12,14 +12,25 @@ HOST = '0.0.0.0'
 PORT = 5555
 
 flag = True
+i = 0
 
 def show_image(frame):
-    cv2.imshow('image', frame)
+    global i
+    path  = "C:\\Users\\rlawk\\OneDrive\\Desktop\\coco\\image" + str(i) + ".jpg"
+
+    with open(path, 'wb') as f:
+        f.write(frame)
+    print('save')
+
+    i += 1
+    i = i % 50
+    print(f'i = {i}')
 
 def send_data(socket, frame):
-    s = 'hello'
+    s = '2'
     s = s.encode()
     socket.send(s)
+    
 
 def main():
     global flag
@@ -34,48 +45,39 @@ def main():
 
         try: 
             while True:
-                
                 print('server 1')
-
                 frame_size_bytes = client_socket.recv(4)
                 frame_size = int.from_bytes(frame_size_bytes, byteorder='big')
-    
+
                 print(frame_size)
 
                 print('server 2')
-
-
-
                 if frame_size == 0:
                     continue
-            
-
+                
                 data = b''
                 while len(data) < frame_size:
                     recv = client_socket.recv(4096)
                     if not recv:
                         break
                     data += recv
-                
+
                 print('server 3')
 
-                _, frame = pickle.loads(data)
-
-
+                frame = pickle.loads(data)
+                
                 print(frame)
+
                 print('server 4')
-
-                if flag:
-                    flag = False
-                    show_image(frame)
-
-                send_data(client_socket, frame)
+                
+                show_image(frame)
 
                 print('server 5')
+                send_data(client_socket, frame)
+
                 frame_size = 0
 
         except Exception as e:
-            print('hello')
             print(e)
         finally:
             client_socket.close()
